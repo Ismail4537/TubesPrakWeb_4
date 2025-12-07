fa13c8b9353bf2536a217b3bb2d7a4337f632578
unknown <deawardini90@gmail.com>
2025-12-07 20:49:19 +0700

    Update dashboard

---
+++ resources/views/dashboard/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.dashboard')
+
+@section('title', 'Dashboard')
+
+@section('content')
+    <h3 class="fw-bold">Dashboard Overview</h3>
+
+    <p>Selamat datang di halaman dashboard.</p>
+@endsection
