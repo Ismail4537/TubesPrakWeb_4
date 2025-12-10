11fcc272e72df82f538dec983eb774b1e61e241a
baxters71 <bgewsder32@gmail.com>
2025-12-10 14:22:00 +0700

    feat/tampilan-tabel-user

---
+++ resources/views/dashboard/category.blade.php
@@ -0,0 +1,12 @@
+<header>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+</header>
+
+<body>
+
+
+
+
+
+</body>
