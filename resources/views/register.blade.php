154fd1182dc0aa1d43bfc6fee7125f4f1ec8b0e0
Dyka21 <andikakhaerulana21@gmail.com>
2025-12-13 20:34:44 +0700

    Feat: Buat struktur dasar halaman register

---
+++ resources/views/register.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Daftar Akun
+    </title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="bg-slate-50 min-h-screen flex items-center justify-center font-sans p-4">
+    
+    </body>
+</html>
