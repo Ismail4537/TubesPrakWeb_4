7c61766d3914ff99242d3d2507146afa55b7888d
Dyka21 <andikakhaerulana21@gmail.com>
2025-12-20 16:51:25 +0700

    feat: membuat struktur awal halaman edit profile

---
+++ resources/views/front-page/edit.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container mx-auto px-4 py-10">
+    {{-- TODO: Nanti mau dibikin layout grid di sini --}}
+    <div class="text-center">
+        <h1 class="text-2xl font-bold text-gray-700">Halaman Edit Profile (Sedang Dibuat)</h1>
+    </div>
+</div>
+@endsection
